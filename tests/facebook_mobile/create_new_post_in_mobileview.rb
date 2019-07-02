b75633a067ba0a878e32b6258c6fcd4caa6054e2
jarapova <arapova.jul2@gmail.com>
2019-07-02 15:46:03 +0300

    annieSelke_click_login

--- tests/facebook_mobile/create_new_post_in_mobileview.rb
+++ tests/facebook_mobile/create_new_post_in_mobileview.rb
@@ -28,43 +28,42 @@ describe 'Preconditions' do
       set_pass pass
     end
 
-      step "User  clicks on the login button for entering into account" do
-        click_login_btn
-      end
-
-      step "User  clicks Ok" do
-        click_ok_btn
-      end
-
-      step "User  clicks on text-block for creating new post" do
-        click_new_post
-      end
-
-      step "User set text" do
-        add_text_in_post
-      end
-
-      step "User clicks on Post button for publishing" do
-        click_post_btn
-      end
-
-      step "Waiting for publishing" do
-        expect_publishing
-      end
-
-      step "User clicks on multiMenu for selecting setting of new post" do
-        click_multi_menu
-      end
-
-      step "User clicks Delete post" do
-        deleting_post
-      end
-
-      step "User is confirms deleting post" do
-        confirm_delete
-      end
-
+    step "User  clicks on the login button for entering into account" do
+      click_login_btn
     end
+
+    step "User  clicks Ok" do
+      click_ok_btn
+    end
+
+    step "User  clicks on text-block for creating new post" do
+      click_new_post
+    end
+
+    step "User set text" do
+      add_text_in_post
+    end
+
+    step "User clicks on Post button for publishing" do
+      click_post_btn
+    end
+
+    step "Waiting for publishing" do
+      expect_publishing
+    end
+
+    step "User clicks on multiMenu for selecting setting of new post" do
+      click_multi_menu
+    end
+
+    step "User clicks Delete post" do
+      deleting_post
+    end
+
+    step "User is confirms deleting post" do
+      confirm_delete
+    end
+  end
 
     scenario 'Searching IL', il_run: true do
 
@@ -78,32 +77,32 @@ describe 'Preconditions' do
 
       step "User set password in password field", settings('facebook')['pass'] do |pass|
         set_pass pass
-        # check_element_path @pass_ta, @pass_il
+        check_element_path @pass_ta, @pass_il
       end
 
       step "User  clicks on the login button for entering into account" do
         click_login_btn
-        # check_element_path @login_btn_ta, @login_btn_il
+        check_element_path @login_btn_ta, @login_btn_il
       end
 
       step "User  clicks Ok" do
         click_ok_btn
-        # check_element_path @ok_btn_ta, @ok_btn_il
+        check_element_path @ok_btn_ta, @ok_btn_il
       end
 
       step "User  clicks on text-block for creating new post" do
         click_new_post
-        # check_element_path @new_post_ta, @new_post_il
+        check_element_path @new_post_ta, @new_post_il
       end
 
       step "User set text" do
         add_text_in_post
-        # check_element_path @set_text_ta, @set_text_il
+        check_element_path @set_text_ta, @set_text_il
       end
 
       step "User clicks on Post button for publishing" do
         click_post_btn
-        # check_element_path @post_btn_ta, @post_btn_il
+        check_element_path @post_btn_ta, @post_btn_il
       end
 
       step "Waiting for publishing" do
@@ -112,17 +111,17 @@ describe 'Preconditions' do
 
       step "User clicks on multiMenu for selecting setting of new post" do
         click_multi_menu
-        # check_element_path @multi_menu_btn_ta, @multi_menu_btn_il
+        check_element_path @multi_menu_btn_ta, @multi_menu_btn_il
       end
 
       step "User clicks Delete post" do
         deleting_post
-        # check_element_path @delete_post_ta, @delete_post_il
+        check_element_path @delete_post_ta, @delete_post_il
       end
 
       step "User is confirms deleting post" do
         confirm_delete
-        # check_element_path @confirm_delete_ta, @confirm_delete_il
+        check_element_path @confirm_delete_ta, @confirm_delete_il
       end
       sleep 3
     end
