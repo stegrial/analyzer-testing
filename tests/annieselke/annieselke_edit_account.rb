1c3ebe5597206d94c77b031ca87ce06f4d28c454
jarapova <arapova.jul2@gmail.com>
2019-07-04 17:58:26 +0300

    annieselke_refactoring

--- tests/annieselke/annieselke_edit_account.rb
+++ tests/annieselke/annieselke_edit_account.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 require_relative '../../pages/annieselke_mobile.rb'
 require_relative '../../helpers/special_methods'
+require_relative '../../pages/united_methods'
 
+include PathCheck
 include AnnieSelkeMobile
 
 describe 'Preconditions' do
 
   before(:all) do
     $caps_chrome['chromeOptions']['mobileEmulation'] = {'deviceName' => 'iPhone 5'}
+  end
+
+  after(:all) do
+    Capybara.current_session.driver.quit
   end
 
   feature 'TA-911 Element not found when the Dom tree is changed (the Login link)' do
@@ -78,60 +84,57 @@ describe 'Preconditions' do
       close_modal
 
       step "humburger menu " do
-        click_humburger_menu
         check_element_path @humburger_menu_ta, @humburger_menu_il
+        click_humburger_menu
       end
 
       step "User clicks on login button" do
-        login_btn
         check_element_path @login_btn_ta, @login_btn_il
+        login_btn
       end
 
-      # sleep 500
-
       step "User set email" do
-        set_email
         check_element_path @email_ta, @email_il
+        set_email
       end
       #
       step "User set password" do
-        set_pass
         check_element_path @pass_ta, @pass_il
+        set_pass
       end
 
       step "User clicks on login button" do
-        click_login_btn
         check_element_path @login_ta, @login_il
+        click_login_btn
       end
 
       step "User clicks on humburger menu" do
-        click_humburger_menu
         check_element_path @humburger_menu_ta, @humburger_menu_il
+        click_humburger_menu
       end
 
       step "User clicks on My Account link" do
-        click_my_acc_btn
         check_element_path @myAccount_ta, @myAccount_il
+        click_my_acc_btn
       end
 
       sleep 5
 
       step "User clicks on personal info button" do
-        click_personal_info_btn
         check_element_path @personal_deteils_ta, @personal_deteils_il
+        click_personal_info_btn
       end
 
       step "User clicks to select gender" do
-        select_gender
         check_element_path @gender_ta, @gender_il
+        select_gender
       end
 
       step "User clicks to select gender" do
-        click_save_btn
         check_element_path @save_btn_ta, @save_btn_il
+        click_save_btn
       end
       sleep 5
-
 
     end
 
@@ -147,45 +150,55 @@ describe 'Preconditions' do
       close_modal
 
       step "humburger menu " do
-        ep_click_humburger_menu
+        check_element_path @humburger_menu_ep, @humburger_menu_il
+        click_humburger_menu true
       end
 
       step "User clicks on login button" do
-        ep_login_btn
+        check_element_path @login_btn_ep, @login_btn_il
+        login_btn true
       end
 
       step "User set email" do
-        ep_set_email
+        check_element_path @email_ep, @email_il
+        set_email true
       end
-
+      #
       step "User set password" do
-        ep_set_pass
+        check_element_path @pass_ep, @pass_il
+        set_pass true
       end
 
       step "User clicks on login button" do
-        ep_click_login_btn
+        check_element_path @login_ep, @login_il
+        click_login_btn true
       end
 
       step "User clicks on humburger menu" do
-        ep_click_humburger_menu
+        check_element_path @humburger_menu_ep, @humburger_menu_il
+        click_humburger_menu true
       end
 
       step "User clicks on My Account link" do
-        ep_click_my_acc_btn
+        check_element_path @myAccount_ep, @myAccount_il
+        click_my_acc_btn true
       end
 
       sleep 5
 
       step "User clicks on personal info button" do
-        ep_click_personal_info_btn
+        check_element_path @personal_deteils_ep, @personal_deteils_il
+        click_personal_info_btn true
       end
 
       step "User clicks to select gender" do
-        ep_select_gender
+        check_element_path @gender_ep, @gender_il
+        select_gender true
       end
 
       step "User clicks to select gender" do
-        ep_click_save_btn
+        check_element_path @save_btn_ep, @save_btn_il
+        click_save_btn true
       end
       sleep 5
     end
@@ -242,7 +255,6 @@ describe 'Preconditions' do
       step "User clicks to select gender" do
         click_save_btn
       end
-      sleep 5
     end
 
     scenario 'Searching debug', search_debug: true do
@@ -295,7 +307,6 @@ describe 'Preconditions' do
       step "User clicks to select gender" do
         click_save_btn
       end
-      sleep 5
     end
   end
 end
