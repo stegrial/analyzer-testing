c4994948629d15f35db7709501301e70c3174f17
Igor Shapovalov <shapovalovii2000@gmail.com>
2019-08-27 18:06:06 +0300

    update tests

--- tests/scope/cloud_bees/create_and_delete_pipeline.rb
+++ tests/scope/cloud_bees/create_and_delete_pipeline.rb
@@ -5,7 +5,6 @@ required_relative_all "/pages/cloud_bees_pages/*.rb"
 
 global_page = CloudBeesGlobal.new
 login_page = CloudBeesLogin.new
-apps_page = CloudBeesApps.new
 pipelines_page = CloudBeesPipelines.new
 
 describe 'Preconditions' do
@@ -342,6 +341,186 @@ describe 'Preconditions' do
       step "Admin clicks on the OK button" do
         check_element_path :xpath, CloudBeesPipelines::CONFIRM_DELETE_PIPELINE_TA, CloudBeesPipelines::CONFIRM_DELETE_PIPELINE_IL
         pipelines_page.confirm_delete_pipeline :ep
+      end
+
+      sleep 3
+    end
+
+    scenario 'Recording debug', record_debug: true do
+      step "User goes to the page", settings('cloud_bees')['login_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin do login", settings('cloud_bees') do |credentials|
+        login_page.fill_username_field :il, credentials['username']
+        login_page.fill_pass_field :il, credentials['pass']
+        login_page.click_sign_in_button :il
+      end
+
+      step "Admin clicks on the Hamburger menu button" do
+        global_page.click_hamburger_menu :il
+      end
+
+      step "Admin chooses the Pipelines section in the Hamburger menu" do
+        global_page.click_pipelines :il
+      end
+
+      step "Admin clicks on the new pipeline button" do
+        pipelines_page.click_new_pipeline :il
+      end
+
+      step "Admin clicks on the create new pipeline button" do
+        pipelines_page.create_new_pipeline :il
+      end
+
+      step "Admin fills the pipeline name field", settings('cloud_bees')['pipeline_name'] do |pipename|
+        pipelines_page.fill_pipeline_name_field :il, pipename
+      end
+
+      step "Admin clicks on the Select project drop-down" do
+        pipelines_page.click_on_select_project :il
+      end
+
+      step "Admin select on the drop-down Default project" do
+        pipelines_page.select_default_project
+      end
+
+      step "Admin confirm new pipeline" do
+        pipelines_page.confirm_create_new_pipeline :il
+      end
+
+      step "Admin clicks 'Add +' for adding task in Stage 1" do
+        pipelines_page.click_new_task :il
+      end
+
+      step "Admin fills the task name field", settings('cloud_bees')['task_name'] do |taskname|
+        pipelines_page.fill_task_name_field :il, taskname
+      end
+
+      step "Admin clicks on the Select Task Type button in the Stage 1" do
+        pipelines_page.select_task_type :il
+      end
+
+      step "Admin clicks on the Manual Task Type in popover" do
+        pipelines_page.manual_task_type :il
+      end
+
+      step "Admin clicks on the Done icon for ending creating new task in Stage 1" do
+        pipelines_page.confirm_new_task :il
+      end
+
+      step "Admin clicks on the Hamburger menu button" do
+        global_page.click_hamburger_menu :il
+      end
+
+      step "Admin chooses the Pipelines section in the Hamburger menu" do
+        global_page.click_pipelines :il
+      end
+
+      step "Admin fills the pipeline name field", settings('cloud_bees')['pipeline_name'] do |pipename|
+        pipelines_page.find_pipeline_name_field :il, pipename
+      end
+
+      step "Admin clicks on new created pipeline" do
+        pipelines_page.choose_pipeline_from_list :il
+      end
+
+      step "Admin clicks on delete button" do
+        pipelines_page.delete_pipeline :il
+      end
+
+      step "Admin clicks on the OK button" do
+        pipelines_page.confirm_delete_pipeline :il
+      end
+
+      sleep 3
+    end
+
+    scenario 'Searching debug', search_debug: true do
+      step "User goes to the page", settings('cloud_bees')['login_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin do login", settings('cloud_bees') do |credentials|
+        login_page.fill_username_field :il, credentials['username']
+        login_page.fill_pass_field :il, credentials['pass']
+        login_page.click_sign_in_button :il
+      end
+
+      step "Admin clicks on the Hamburger menu button" do
+        global_page.click_hamburger_menu :il
+      end
+
+      step "Admin chooses the Pipelines section in the Hamburger menu" do
+        global_page.click_pipelines :il
+      end
+
+      step "Admin clicks on the new pipeline button" do
+        pipelines_page.click_new_pipeline :il
+      end
+
+      step "Admin clicks on the create new pipeline button" do
+        pipelines_page.create_new_pipeline :il
+      end
+
+      step "Admin fills the pipeline name field", settings('cloud_bees')['pipeline_name'] do |pipename|
+        pipelines_page.fill_pipeline_name_field :il, pipename
+      end
+
+      step "Admin clicks on the Select project drop-down" do
+        pipelines_page.click_on_select_project :il
+      end
+
+      step "Admin select on the drop-down Default project" do
+        pipelines_page.select_default_project
+      end
+
+      step "Admin confirm new pipeline" do
+        pipelines_page.confirm_create_new_pipeline :il
+      end
+
+      step "Admin clicks 'Add +' for adding task in Stage 1" do
+        pipelines_page.click_new_task :il
+      end
+
+      step "Admin fills the task name field", settings('cloud_bees')['task_name'] do |taskname|
+        pipelines_page.fill_task_name_field :il, taskname
+      end
+
+      step "Admin clicks on the Select Task Type button in the Stage 1" do
+        pipelines_page.select_task_type :il
+      end
+
+      step "Admin clicks on the Manual Task Type in popover" do
+        pipelines_page.manual_task_type :il
+      end
+
+      step "Admin clicks on the Done icon for ending creating new task in Stage 1" do
+        pipelines_page.confirm_new_task :il
+      end
+
+      step "Admin clicks on the Hamburger menu button" do
+        global_page.click_hamburger_menu :il
+      end
+
+      step "Admin chooses the Pipelines section in the Hamburger menu" do
+        global_page.click_pipelines :il
+      end
+
+      step "Admin fills the pipeline name field", settings('cloud_bees')['pipeline_name'] do |pipename|
+        pipelines_page.find_pipeline_name_field :il, pipename
+      end
+
+      step "Admin clicks on new created pipeline" do
+        pipelines_page.choose_pipeline_from_list :il
+      end
+
+      step "Admin clicks on delete button" do
+        pipelines_page.delete_pipeline :il
+      end
+
+      step "Admin clicks on the OK button" do
+        pipelines_page.confirm_delete_pipeline :il
       end
 
       sleep 3
