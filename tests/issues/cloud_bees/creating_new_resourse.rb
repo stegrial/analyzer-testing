f14d06597266634bea799bfba8f093160f9ef2d5
jarapova <arapova.jul2@gmail.com>
2019-09-04 14:02:33 +0300

    TA-1016

---
+++ tests/issues/cloud_bees/creating_new_resourse.rb
@@ -0,0 +1,184 @@
+require 'spec_helper'
+require_relative '../../../helpers/special_methods'
+require_relative '../../../pages/united_methods'
+required_relative_all "/pages/cloud_bees_pages/*.rb"
+
+login_page = CloudBeesLogin.new
+resources_page = CloudBeesResources.new
+
+describe 'Preconditions' do
+
+  before(:all) do
+    $caps_chrome['goog:chromeOptions'].delete('mobileEmulation')
+    Capybara.page.driver.browser.manage.window.resize_to(1440,800)
+
+  end
+
+  after(:all) do
+    Capybara.current_session.driver.quit
+  end
+  feature 'TA-1016 CloudBees - Creating new resource' do
+
+    # Initial locators with Recording
+
+    scenario 'Recording IL', il: true do
+      step "User goes to the page", settings('cloud_bees')['login_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin do login", settings('cloud_bees') do |credentials|
+        login_page.fill_username_field credentials['username']
+        login_page.fill_pass_field credentials['pass']
+        login_page.click_sign_in_button
+      end
+
+      step "User goes to the page", settings('cloud_bees')['resource_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin clicks Add button for creating new resource" do
+        resources_page.click_to_add_new_resource
+      end
+      step "Admin clicks create new resource button" do
+        resources_page.click_create_resource_btn
+      end
+
+      step "Admin set name of new resource input" do
+        resources_page.set_name_of_resource
+      end
+
+      sleep 4
+    end
+
+    scenario 'Searching IL', il1: true do
+      step "User goes to the page", settings('cloud_bees')['login_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin do login", settings('cloud_bees') do |credentials|
+        check_element_path :css, CloudBeesLogin::USERNAME_FIELD_TA, CloudBeesLogin::USERNAME_FIELD_IL
+        login_page.fill_username_field  credentials['username']
+
+        check_element_path :css, CloudBeesLogin::PASSWORD_FIELD_TA, CloudBeesLogin::PASSWORD_FIELD_IL
+        login_page.fill_pass_field  credentials['pass']
+
+        check_element_path :css, CloudBeesLogin::SIGN_IN_BTN_TA, CloudBeesLogin::SIGN_IN_BTN_IL
+        login_page.click_sign_in_button
+      end
+
+      step "User goes to the page", settings('cloud_bees')['resource_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin clicks Add button for creating new resource" do
+        check_element_path :xpath, CloudBeesResources::ADD_NEW_RESOURCE_TA, CloudBeesResources::ADD_NEW_RESOURCE_IL
+        resources_page.click_to_add_new_resource
+      end
+
+      step "Admin clicks create new resource button" do
+        check_element_path :xpath, CloudBeesResources::CREATE_RESOURCE_BTN_TA, CloudBeesResources::CREATE_RESOURCE_BTN_IL
+        resources_page.click_create_resource_btn
+      end
+
+      step "Admin set name of new resource input" do
+        check_element_path :xpath, CloudBeesResources::RESOURCE_NAME_TA, CloudBeesResources::RESOURCE_NAME_IL
+        resources_page.set_name_of_resource
+      end
+    end
+
+    # Element Picker from Repository
+
+    scenario 'Searching EP', ep: true do
+      step "User goes to the page", settings('cloud_bees')['login_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin do login", settings('cloud_bees') do |credentials|
+        check_element_path :css, CloudBeesLogin::USERNAME_FIELD_EP, CloudBeesLogin::USERNAME_FIELD_IL
+        login_page.fill_username_field :ep,  credentials['username']
+
+        check_element_path :css, CloudBeesLogin::PASSWORD_FIELD_EP, CloudBeesLogin::PASSWORD_FIELD_IL
+        login_page.fill_pass_field :ep, credentials['pass']
+
+        check_element_path :css, CloudBeesLogin::SIGN_IN_BTN_EP, CloudBeesLogin::SIGN_IN_BTN_IL
+        login_page.click_sign_in_button :ep
+      end
+
+      step "User goes to the page", settings('cloud_bees')['resource_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin clicks Add button for creating new resource" do
+        check_element_path :xpath, CloudBeesResources::ADD_NEW_RESOURCE_EP, CloudBeesResources::ADD_NEW_RESOURCE_IL
+        resources_page.click_to_add_new_resource :ep
+      end
+
+      step "Admin clicks create new resource button" do
+        check_element_path :xpath, CloudBeesResources::CREATE_RESOURCE_BTN_EP, CloudBeesResources::CREATE_RESOURCE_BTN_IL
+        resources_page.click_create_resource_btn :ep
+      end
+
+      step "Admin set name of new resource input" do
+        check_element_path :xpath, CloudBeesResources::RESOURCE_NAME_EP, CloudBeesResources::RESOURCE_NAME_IL
+        resources_page.set_name_of_resource :ep
+      end
+
+    end
+
+    scenario 'Recording debug', record_debug: true do
+      step "User goes to the page", settings('cloud_bees')['login_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin do login", settings('cloud_bees') do |credentials|
+        login_page.fill_username_field :il, credentials['username']
+        login_page.fill_pass_field :il, credentials['pass']
+        login_page.click_sign_in_button :il
+      end
+
+      step "User goes to the page", settings('cloud_bees')['resource_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin clicks Add button for creating new resource" do
+        resources_page.click_to_add_new_resource :il
+      end
+      step "Admin clicks create new resource button" do
+        resources_page.click_create_resource_btn :il
+      end
+
+      step "Admin set name of new resource input" do
+        resources_page.set_name_of_resource
+      end
+
+      sleep 3
+    end
+
+    scenario 'Searching debug', search_debug: true do
+      step "User goes to the page", settings('cloud_bees')['login_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin do login", settings('cloud_bees') do |credentials|
+        login_page.fill_username_field :il, credentials['username']
+        login_page.fill_pass_field :il, credentials['pass']
+        login_page.click_sign_in_button :il
+      end
+      step "User goes to the page", settings('cloud_bees')['resource_page'] do |url|
+        page.visit url
+      end
+
+      step "Admin clicks Add button for creating new resource" do
+        resources_page.click_to_add_new_resource :il
+      end
+      step "Admin clicks create new resource button" do
+        resources_page.click_create_resource_btn :il
+      end
+
+      step "Admin set name of new resource input" do
+        check_element_path :xpath, CloudBeesResources::RESOURCE_NAME_TA, CloudBeesResources::RESOURCE_NAME_IL
+        resources_page.set_name_of_resource
+      end
+    end
+  end
+end
