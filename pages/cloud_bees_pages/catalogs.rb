873bf03d08c1b57dabda962f72f60a07a52804a4
jarapova <arapova.jul2@gmail.com>
2019-09-10 14:36:56 +0300

    test2

--- pages/cloud_bees_pages/catalogs.rb
+++ pages/cloud_bees_pages/catalogs.rb
@@ -302,4 +302,8 @@ class CloudBeesCatalogs
     return find(:css, UTILITY_IN_CATALOG_LIST_IL).click if key == :il
     find(:css, ta(UTILITY_IN_CATALOG_LIST_TA, UTILITY_IN_CATALOG_LIST_IL)).click
   end
+
+
+
+  
 end
