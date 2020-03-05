5ce4aae4122962a2c793dd9d1bebc286aba38ff6
vbykadorov <vbykadorov@cloudbees.com>
2020-03-05 23:52:16 +0200

    updated scenarios

--- pages/planetblue.rb
+++ pages/planetblue.rb
@@ -9,7 +9,7 @@ class PlanetBlue
   include PageExtension
 
   def ta_name(name)
-    name.tr('^A-Za-z0-9_|', '')
+    name.tr('0-9_ ', '').tr('|', '').tr('A-Z', 'a-z')
   end
 
   def locator_by_type(locator, initial_locator, ta_locator)
@@ -20,20 +20,32 @@ class PlanetBlue
     end
   end
 
+  def find_element_path(key, format, initialLocator, taLocator)
+    post_processing key do
+      return find(format, initialLocator) if key == :il
+      find(format, ta(taLocator, initialLocator))
+    end
+  end
+
   MAIN_MENU_BUTTON_IL = "[aria-label='Menu']"
   MAIN_MENU_BUTTON_TA = "planetblue:menu_button"
 
   def menu_category(locator, name)
-    case locator
-    when :il then "//div[@role='button']/div[text()='#{name}']"
-    when :ta then "planetblue:menu_button:#{ta_name(name)}"
-    else p 'Locator type is not set'
-    end
+    locator_by_type locator,
+                    "//div[@role='button']/div[text()='#{name}']",
+                    "planetblue:menu_button:#{ta_name(name)}"
+  end
+
+  def category_dropdown(locator, name)
+    locator_by_type locator,
+                    "//button/span/span[text()='#{name}']",
+                    "planetblue:category_dropdown:#{ta_name(name)}"
   end
 
   def collection_item(name)
     "//li//a[@aria-label='#{name}']//ancestor::li"
   end
+
 
   def collection_item_link(locator, name)
     locator_by_type locator,
@@ -55,13 +67,13 @@ class PlanetBlue
 
   def collection_item_price(locator, name)
     locator_by_type locator,
-                    "#{collection_item name}//span[contains('text', '$')]",
+                    "#{collection_item name}//span[contains(text(), '$')]",
                     "planetblue:collection_item:#{ta_name(name)}:price"
   end
 
-  def collection_item_color(locator, name)
+  def collection_item_color(locator, name, color)
     locator_by_type locator,
-                    "#{collection_item name}//div[contains(@amp-bind, 'color.selected.id')",
+                    "#{collection_item name}//div[contains(@amp-bind, 'color.selected.id')]//img[@alt='#{color}']",
                     "planetblue:collection_item:#{ta_name(name)}:color"
   end
 
@@ -82,17 +94,17 @@ class PlanetBlue
 
   def find_category_dropdown(key = nil, name)
     post_processing key do
-      return find(:xpath, menu_category(:il, name)) if key == :il
-      find(:xpath, ta(menu_category(:ta, name), menu_category(:il, name)))
+      return find(:xpath, category_dropdown(:il, name)) if key == :il
+      find(:xpath, ta(category_dropdown(:ta, name), category_dropdown(:il, name)))
     end
   end
 
-  def check_collection_item_attributes(key = nil, name, title, price, color)
-    post_processing key do
-      find(:xpath, collection_item_link(key, name))
-      expect(collection_item_title key, name).to have_attributes('')
-    end
-
+  def check_collection_item(key = nil, name, color)
+        find_element_path(key, :xpath, collection_item_link(:il, name), collection_item_link(:ta, name))
+        find_element_path(key, :xpath, collection_item_title(:il, name), collection_item_title(:ta, name))
+        find_element_path(key, :xpath, collection_item_image(:il, name), collection_item_image(:ta, name))
+        find_element_path(key, :xpath, collection_item_price(:il, name), collection_item_price(:ta, name))
+        find_element_path(key, :xpath, collection_item_color(:il, name, color), collection_item_color(:ta, name, color))
   end
 
 end
--- tests/scope/planetblue/planetblue_category_selecting.rb
+++ tests/scope/planetblue/planetblue_category_selecting.rb
@@ -38,44 +38,66 @@ describe 'Preconditions' do
         it.click_menu_category value
       end
 
+      step "User clicks on Menu Category", 'Basics' do |value|
+        it.click_menu_category value
+      end
+
       step "User checks Category Dropdowns", 'REFINE', 'Sort' do |value1, value2|
         it.find_category_dropdown value1
         it.find_category_dropdown value2
       end
 
-      step "User checks Collection Item",
-           'RUTH TUNIC | NEW', 'BLUE LIFE', '150.00', 'Bright Rose' do |name, title, price, color|
-        it.check_collection_item_attributes name, title, price, color
+      step "User checks Collection Items", 'RUTH TUNIC | New', 'Bright Rose' do |name, color|
+        it.check_collection_item name, color
       end
 
     end
 
-    # scenario 'Searching IL', il: true do
+    scenario 'Searching IL', il: true do
 
-      # step "User goes to the page", settings('olx')['page'] do |url|
-      #   page.visit url
-      # end
-      #
-      # step "User clicks on animal category button" do
-      #   check_element_path :xpath, Olx::ANIMAL_CATEGORY_TA, Olx::ANIMAL_CATEGORY_IL
-      #   it.click_menu_button
-      # end
+      step "User goes to the page", settings('planetblue')['page'] do |url|
+        page.visit url
+      end
 
-      # step "User clicks on free animal matting button" do
-      #   check_element_path :xpath, Olx::FREE_ANIMAL_MATING_TA, Olx::FREE_ANIMAL_MATING_IL
-      #   it.click_free_animal_matting
-      # end
-      #
-      # step "User clicks on gallary view button" do
-      #   check_element_path :xpath, Olx::GALLERY_VIEW_TA, Olx::GALLERY_VIEW_IL
-      #   it.click_gallery_view
-      # end
-      #
-      # step "User clicks to select product" do
-      #   check_element_path :xpath, Olx::PRODUCT_TA, Olx::PRODUCT_IL
-      #   it.click_to_select_product
-      # end
-    # end
+      step "User clicks on Menu button" do
+        check_element_path :css, PlanetBlue::MAIN_MENU_BUTTON_TA, PlanetBlue::MAIN_MENU_BUTTON_IL
+        it.click_menu_button
+      end
+
+      step "User clicks on Menu Category", 'Clothing' do |value|
+        check_element_path :xpath, it.menu_category(:ta, value), it.menu_category(:il, value)
+        it.click_menu_category value
+      end
+
+      step "User clicks on Menu Category", 'Tops' do |value|
+        check_element_path :xpath, it.menu_category(:ta, value), it.menu_category(:il, value)
+        it.click_menu_category value
+      end
+
+      step "User clicks on Menu Category", 'Basics' do |value|
+        check_element_path :xpath, it.menu_category(:ta, value), it.menu_category(:il, value)
+        it.click_menu_category value
+      end
+
+      step "User checks Category Dropdowns", 'REFINE', 'Sort' do |value1, value2|
+        check_element_path :xpath, it.category_dropdown(:ta, value1), it.category_dropdown(:il, value1)
+        check_element_path :xpath, it.category_dropdown(:ta, value2), it.category_dropdown(:il, value2)
+        it.find_category_dropdown value1
+        it.find_category_dropdown value2
+      end
+
+      step "User checks Collection Items", 'RUTH TUNIC | New', 'Bright Rose' do |name, color|
+
+        check_element_path(:xpath, it.collection_item_link(:il, name), it.collection_item_link(:ta, name))
+        check_element_path(:xpath, it.collection_item_title(:il, name), it.collection_item_title(:ta, name))
+        check_element_path(:xpath, it.collection_item_image(:il, name), it.collection_item_image(:ta, name))
+        check_element_path(:xpath, it.collection_item_price(:il, name), it.collection_item_price(:ta, name))
+        check_element_path(:xpath, it.collection_item_color(:il, name, color), it.collection_item_color(:ta, name, color))
+
+        it.check_collection_item name, color
+      end
+
+    end
 
   end
 end
