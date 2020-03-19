4008b2766a08a030a9b8db804724997c3ea8ea0d
Ed Serbin <edserbin2@gmail.com>
2020-03-19 12:16:17 +0200

    added new elements on youtube page

--- pages/youtube.rb
+++ pages/youtube.rb
@@ -25,6 +25,25 @@ class Youtube
   VIDEO_FROM_SEARCH_2_IL = "(//div[@id='grid-container']/ytd-video-renderer)[1]/div[@id='dismissable']/ytd-thumbnail/a[@id='thumbnail']"
   VIDEO_FROM_SEARCH_2_TA = "youtube:first_video2"
 
+  SETTINGS_MENU_BUTTON_IL = "(//div[@id='buttons']//div[@id='button'])[3]"
+  SETTINGS_MENU_BUTTON_TA = "youtube:setting_menu_button"
+
+  DARK_THEME_MENU_IL = "//iron-dropdown/div[@id='contentWrapper']/ytd-multi-page-menu-renderer//div[@id='items']//div[@id='label']"
+  DARK_THEME_MENU_TA = "youtube:dark_theme_menu"
+
+  DARK_THEME_BUTTON_IL = "//div[@id='toggleButton']"
+  DARK_THEME_BUTTON_TA = "youtube:theme_toggle_button"
+
+  MENU_BACK_ARROW_IL = "//ytd-button-renderer[@id='back-button']//button[@id='button']"
+  MENU_BACK_ARROW_TA = "youtube:menu_back_button"
+
+  LANGUAGE_MENU_IL = "//div[@id='settings']//paper-item[1]"
+  LANGUAGE_MENU_TA = "youtube:language_menu"
+
+  DEUTSCH_BUTTON_IL = "//p[text()='Deutsch']"
+  DEUTSCH_BUTTON_TA = "youtube:deutsch"
+
+
   def set_data_in_search(key = nil, data)
     find_element_path(key, :xpath, YOUTUBE_SEARCH_TA, YOUTUBE_SEARCH_IL).fill_in with: data
   end
@@ -49,8 +68,28 @@ class Youtube
     find_element_path(key, :xpath, VIDEO_FROM_SEARCH_2_TA, VIDEO_FROM_SEARCH_2_IL).click
   end
 
-  def use_element(key = nil, element)
-    find_element_path(key, :xpath, YOUTUBE_BUTTON_SEARCH_TA, YOUTUBE_BUTTON_SEARCH_IL).click
+  def click_setting_menu(key = nil)
+    find_element_path(key, :xpath, SETTINGS_MENU_BUTTON_TA, SETTINGS_MENU_BUTTON_IL).click
+  end
+
+  def click_theme_menu(key = nil)
+    find_element_path(key, :xpath, DARK_THEME_MENU_TA, DARK_THEME_MENU_IL).click
+  end
+
+  def change_theme(key = nil)
+    find_element_path(key, :xpath, DARK_THEME_BUTTON_TA, DARK_THEME_BUTTON_IL).click
+  end
+
+  def click_back_button_in_settings(key = nil)
+    find_element_path(key, :xpath, MENU_BACK_ARROW_TA, MENU_BACK_ARROW_IL).click
+  end
+
+  def click_language_menu(key = nil)
+    find_element_path(key, :xpath, LANGUAGE_MENU_TA, LANGUAGE_MENU_IL).click
+  end
+
+  def select_deutsch(key = nil)
+    find_element_path(key, :xpath, DEUTSCH_BUTTON_TA, DEUTSCH_BUTTON_IL).click
   end
 
 end
