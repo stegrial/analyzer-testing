36eb167cf91a5274f8f4be54c8f810d914314c88
vasiliyvg <vbykadorov@softesis.com>
2020-03-27 17:44:48 +0200

    added search scenarios for tests: - planetblue_category_selecting_spec.rb - planetblue_change_shipping_address_spec.rb - planetblue_contact_us_page_spec.rb

--- spec/scope/planetblue/planetblue_change_shipping_address_spec.rb
+++ spec/scope/planetblue/planetblue_change_shipping_address_spec.rb
@@ -40,5 +40,33 @@ describe 'Preconditions' do
 
     end
 
+    scenario 'Searching Locators', record: true do
+
+      step "User goes to the page", settings('planetblue')['page'] + 'pages/student-discounts' do |url|
+        page.visit url
+      end
+
+      step "User clicks Shipping dropdown" do
+        check_element_path :css, shipping.SHIPPING_DROPDOWN_TA, shipping.SHIPPING_DROPDOWN_IL
+        shipping.click_shipping_dropdown :ta
+      end
+
+      step "User selects Country from dropdown" do
+        check_element_path :css, shipping.COUNTRY_DROPDOWN_TA, shipping.COUNTRY_DROPDOWN_IL
+                           shipping.select_country :ta, 'Zambia'
+      end
+
+      step "User selects currency from dropdown" do
+        check_element_path :css, shipping.CURRENCY_DROPDOWN_TA, shipping.CURRENCY_DROPDOWN_IL
+        shipping.select_currency :ta, 'US Dollar'
+      end
+
+      step "User clicks Save button" do
+        check_element_path :css, shipping.SAVE_BUTTON_TA, shipping.SAVE_BUTTON_IL
+        shipping.click_save_button :ta
+      end
+
+    end
+
   end
 end
