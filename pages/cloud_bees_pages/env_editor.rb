d8712b067dc68957b8831635209748d5907d295a
Igor Shapovalov <shapovalovii2000@gmail.com>
2020-05-06 10:59:28 +0300

    update pages for CloudBees site

--- pages/cloud_bees_pages/env_editor.rb
+++ pages/cloud_bees_pages/env_editor.rb
@@ -9,110 +9,66 @@ class CloudBeesEnvEditor
 
   RESOURCE_TIER_IL = "(//div[@class='tier-inner'])[1]"
   RESOURCE_TIER_TA = 'cloud_bees:env_editor:resource_tier'
-  RESOURCE_TIER_EP = 'EP:cloud_bees:env_editor:resource_tier'
 
   ADD_RESOURCE_BTN_IL = ".at-add-resource"
   ADD_RESOURCE_BTN_TA = "cloud_bees:env_editor:add_resource"
-  ADD_RESOURCE_BTN_EP = "EP:cloud_bees:env_editor:add_resource"
 
   SELECT_RESOURCE_FROM_LIST_IL = "//span[contains(text(), 'resource_for_mapping_test')]"
   SELECT_RESOURCE_FROM_LIST_TA = "cloud_bees:env_editor:resource_from_list"
-  SELECT_RESOURCE_FROM_LIST_EP = "EP:cloud_bees:env_editor:resource_from_list"
 
   MAP_TO_APP_BTN_IL = ".at-hierarchy-nav-setup-environment-map-block"
   MAP_TO_APP_BTN_TA = "cloud_bees:env_editor:map_to_app_hierarchy_menu"
-  MAP_TO_APP_BTN_EP = "EP:cloud_bees:env_editor:map_to_app_hierarchy_menu"
 
   APPNAME_IN_SEARCH_FIELD_IL = ".at-search-collapsable-input"
   APPNAME_IN_SEARCH_FIELD_TA = "cloud_bees:env_editor:app_from_search"
-  APPNAME_IN_SEARCH_FIELD_EP = "EP:cloud_bees:env_editor:app_from_search"
 
   APPS_NAME_IN_MAPPING_MODAL_IL = ".ec-grid-primary__row--pointer"
   APPS_NAME_IN_MAPPING_MODAL_TA = "cloud_bees:env_editor:app_from_maplist"
-  APPS_NAME_IN_MAPPING_MODAL_EP = "EP:cloud_bees:env_editor:app_from_maplist"
 
   MAPPING_SELECT_BTN_IL = ".at-mapping-select-btn"
   MAPPING_SELECT_BTN_TA = "cloud_bees:env_editor:mapping_select_btn"
-  MAPPING_SELECT_BTN_EP = "EP:cloud_bees:env_editor:mapping_select_btn"
 
   TIER_MAPPING_POPOVER_IL = ".at-popover-not-link-undefined"
   TIER_MAPPING_POPOVER_TA = "cloud_bees:env_editor:tier_map"
-  TIER_MAPPING_POPOVER_EP = "EP:cloud_bees:env_editor:tier_map"
 
   CLOSE_BTN_IL = ".at-ok-btn"
   CLOSE_BTN_TA = "cloud_bees:env_editor:close_btn"
-  CLOSE_BTN_EP = "EP:cloud_bees:env_editor:close_btn"
 
 
   def click_resource_tier(key = nil)
-    post_processing key do
-      return find(ta(RESOURCE_TIER_EP)).click if key == :ep
-      return find(:xpath, RESOURCE_TIER_IL).click if key == :il
-      find(:xpath, ta(RESOURCE_TIER_TA, RESOURCE_TIER_IL)).click
-    end
+    find_element(key, il_type: :xpath, tl: RESOURCE_TIER_TA, il: RESOURCE_TIER_IL).click
   end
 
   def click_to_add_resource(key = nil)
-    post_processing key do
-      return find(ta(ADD_RESOURCE_BTN_EP)).click if key == :ep
-      return find(:css, ADD_RESOURCE_BTN_IL).click if key == :il
-      find(:css, ta(ADD_RESOURCE_BTN_TA, ADD_RESOURCE_BTN_IL)).click
-    end
+    find_element(key, il_type: :css, tl: ADD_RESOURCE_BTN_TA, il: ADD_RESOURCE_BTN_IL).click
   end
 
   def select_resource_from_list(key = nil)
-    post_processing key do
-      return find(ta(SELECT_RESOURCE_FROM_LIST_EP)).click if key == :ep
-      return find(:xpath, SELECT_RESOURCE_FROM_LIST_IL).click if key == :il
-      find(:xpath, ta(SELECT_RESOURCE_FROM_LIST_TA, SELECT_RESOURCE_FROM_LIST_IL)).click
-    end
+    find_element(key, il_type: :xpath, tl: SELECT_RESOURCE_FROM_LIST_TA, il: SELECT_RESOURCE_FROM_LIST_IL).click
   end
 
   def click_map_to_app_hierarchy_menu(key = nil)
-    post_processing key do
-      return find(ta(MAP_TO_APP_BTN_EP)).click if key == :ep
-      return find(:css, MAP_TO_APP_BTN_IL).click if key == :il
-      find(:css, ta(MAP_TO_APP_BTN_TA, MAP_TO_APP_BTN_IL)).click
-    end
+    find_element(key, il_type: :css, tl: MAP_TO_APP_BTN_TA, il: MAP_TO_APP_BTN_IL).click
   end
 
   def set_app_in_search(key = nil, appName)
-    post_processing key do
-      return find(ta(APPNAME_IN_SEARCH_FIELD_EP)).set(appName) if key == :ep
-      return find(:css, APPNAME_IN_SEARCH_FIELD_IL).set(appName) if key == :il
-      find(:css, ta(APPNAME_IN_SEARCH_FIELD_TA, APPNAME_IN_SEARCH_FIELD_IL)).set(appName)
-    end
+    find_element(key, il_type: :css, tl: APPNAME_IN_SEARCH_FIELD_TA, il: APPNAME_IN_SEARCH_FIELD_IL).set(appName)
   end
 
   def select_app_from_maplist(key = nil)
-    post_processing key do
-      return find(ta(APPS_NAME_IN_MAPPING_MODAL_EP)).click if key == :ep
-      return find(:css, APPS_NAME_IN_MAPPING_MODAL_IL).click if key == :il
-      find(:css, ta(APPS_NAME_IN_MAPPING_MODAL_TA, APPS_NAME_IN_MAPPING_MODAL_IL)).click
-    end
+    find_element(key, il_type: :css, tl: APPS_NAME_IN_MAPPING_MODAL_TA, il: APPS_NAME_IN_MAPPING_MODAL_IL).click
   end
+
   def click_mapping_select_btn(key = nil)
-    post_processing key do
-      return find(ta(MAPPING_SELECT_BTN_EP)).click if key == :ep
-      return find(:css, MAPPING_SELECT_BTN_IL).click if key == :il
-      find(:css, ta(MAPPING_SELECT_BTN_TA, MAPPING_SELECT_BTN_IL)).click
-    end
+    find_element(key, il_type: :css, tl: MAPPING_SELECT_BTN_TA, il: MAPPING_SELECT_BTN_IL).click
   end
 
   def click_to_select_tier_for_map(key = nil)
-    post_processing key do
-      return find(ta(TIER_MAPPING_POPOVER_EP)).click if key == :ep
-      return find(:css, TIER_MAPPING_POPOVER_IL).click if key == :il
-      find(:css, ta(TIER_MAPPING_POPOVER_TA, TIER_MAPPING_POPOVER_IL)).click
-    end
+    find_element(key, il_type: :css, tl: TIER_MAPPING_POPOVER_TA, il: TIER_MAPPING_POPOVER_IL).click
   end
 
   def click_to_close_to_close_modal(key = nil)
-    post_processing key do
-      return find(ta(CLOSE_BTN_EP)).click if key == :ep
-      return find(:css, CLOSE_BTN_IL).click if key == :il
-      find(:css, ta(CLOSE_BTN_TA, CLOSE_BTN_IL)).click
-    end
+    find_element(key, il_type: :css, tl: CLOSE_BTN_TA, il: CLOSE_BTN_IL).click
   end
 
 end
--- pages/cloud_bees_pages/global.rb
+++ pages/cloud_bees_pages/global.rb
@@ -9,109 +9,66 @@ class CloudBeesGlobal
 
   HAM_MENU_BTN_IL = '.at-main-menu-btn'
   HAM_MENU_BTN_TA = 'cloud_bees:ham_menu'
-  HAM_MENU_BTN_EP = 'EP:cloud_bees:ham_menu'
 
   USERS_SECTION_IL = "//a[./span[text()='Users']]"
   USERS_SECTION_TA = 'cloud_bees:ham_menu:users'
-  USERS_SECTION_EP = 'EP:cloud_bees:ham_menu:users'
 
   PIPELINES_SECTION_IL = '.at-main-menu-pipelines'
   PIPELINES_SECTION_TA = "cloud_bees:pipelines"
-  PIPELINES_SECTION_EP = "EP:cloud_bees:pipelines"
 
   APPS_SECTION_IL = "a.at-main-menu-applications"
   APPS_SECTION_TA = "cloud_bees:applications"
-  APPS_SECTION_EP = "EP:cloud_bees:applications"
 
   ENV_SECTION_IL = "a.at-main-menu-environments"
   ENV_SECTION_TA = "cloud_bees:environments"
-  ENV_SECTION_EP = "EP:cloud_bees:environments"
 
   ADMIN_MENU_IL = "//div[@class='user at-user-logged-in']"
   ADMIN_MENU_TA = "cloud_bees:admin_menu"
-  ADMIN_MENU_EP = "EP:cloud_bees:admin_menu"
 
   LOGOUT_BTN_IL = "//div[@class='user-menu-item at-user-menu-item-logout']"
   LOGOUT_BTN_TA = "cloud_bees:admin_menu:logout"
-  LOGOUT_BTN_EP = "EP:cloud_bees:admin_menu:logout"
 
   RELEASE_CALENDAR_BTN_IL = ".at-icon-nav-item-releases-calendar"
   RELEASE_CALENDAR_BTN_TA = "cloud_bees:admin_menu:release_calendar_btn"
-  RELEASE_CALENDAR_BTN_EP = "EP:cloud_bees:admin_menu:release_calendar_btn"
 
   CHANGE_HISTORY_SECTION_IL = '.at-main-menu-change_history'
   CHANGE_HISTORY_SECTION_TA = 'cloud_bees:change_history'
-  CHANGE_HISTORY_SECTION_EP = 'EP:cloud_bees:change_history'
+
 
   def click_hamburger_menu(key = nil)
-    post_processing key do
-      return find(ta(HAM_MENU_BTN_EP)).click if key == :ep
-      return find(:css, HAM_MENU_BTN_IL).click if key == :il
-      find(:css, ta(HAM_MENU_BTN_TA, HAM_MENU_BTN_IL)).click
-    end
+    find_element(key, il_type: :css, tl: HAM_MENU_BTN_TA, il: HAM_MENU_BTN_IL).click
   end
 
   def choose_users_section(key = nil)
-    post_processing key do
-      return find(ta(USERS_SECTION_EP)).click if key == :ep
-      return find(:xpath, USERS_SECTION_IL).click if key == :il
-      find(:xpath, ta(USERS_SECTION_TA, USERS_SECTION_IL)).click
-    end
+    find_element(key, il_type: :xpath, tl: USERS_SECTION_TA, il: USERS_SECTION_IL).click
   end
 
   def click_pipelines(key = nil)
-    post_processing key do
-      return find(ta(PIPELINES_SECTION_EP)).click if key == :ep
-      return find(:css, PIPELINES_SECTION_IL).click if key == :il
-      find(:css, ta(PIPELINES_SECTION_TA, PIPELINES_SECTION_IL)).click
-    end
+    find_element(key, il_type: :css, tl: PIPELINES_SECTION_TA, il: PIPELINES_SECTION_IL).click
   end
 
   def click_applications(key = nil)
-    post_processing key do
-      return find(ta(APPS_SECTION_EP)).click if key == :ep
-      return find(:css, APPS_SECTION_IL).click if key == :il
-      find(:css, ta(APPS_SECTION_TA, APPS_SECTION_IL)).click
-    end
+    find_element(key, il_type: :css, tl: APPS_SECTION_TA, il: APPS_SECTION_IL).click
   end
 
   def click_environments(key = nil)
-    post_processing key do
-      return find(ta(ENV_SECTION_EP)).click if key == :ep
-      return find(:css, ENV_SECTION_IL).click if key == :il
-      find(:css, ta(ENV_SECTION_TA, ENV_SECTION_IL)).click
-    end
+    find_element(key, il_type: :css, tl: ENV_SECTION_TA, il: ENV_SECTION_IL).click
   end
 
   def click_admin_menu(key = nil)
-    post_processing key do
-      return find(ta(ADMIN_MENU_EP)).click if key == :ep
-      return find(:xpath, ADMIN_MENU_IL).click if key == :il
-      find(:xpath, ta(ADMIN_MENU_TA, ADMIN_MENU_IL)).click
-    end
+    find_element(key, il_type: :xpath, tl: ADMIN_MENU_TA, il: ADMIN_MENU_IL).click
   end
 
   def click_logout_button(key = nil)
-    post_processing key do
-      return find(ta(LOGOUT_BTN_EP)).click if key == :ep
-      return find(:xpath, LOGOUT_BTN_IL).click if key == :il
-      find(:xpath, ta(LOGOUT_BTN_TA, LOGOUT_BTN_IL)).click
-    end
+    find_element(key, il_type: :xpath, tl: LOGOUT_BTN_TA, il: LOGOUT_BTN_IL).click
   end
 
   def click_release_calendar_btn(key = nil)
-    post_processing key do
-      return find(ta(RELEASE_CALENDAR_BTN_EP)).click if key == :ep
-      return find(:css, RELEASE_CALENDAR_BTN_IL).click if key == :il
-      find(:css, ta(RELEASE_CALENDAR_BTN_TA, RELEASE_CALENDAR_BTN_IL)).click
-    end
+    find_element(key, il_type: :css, tl: RELEASE_CALENDAR_BTN_TA, il: RELEASE_CALENDAR_BTN_IL).click
   end
 
   def click_change_history(key = nil)
-    post_processing key do
-      return find(ta(CHANGE_HISTORY_SECTION_EP)).click if key == :ep
-      return find(:css, CHANGE_HISTORY_SECTION_IL).click if key == :il
-      find(:css, ta(CHANGE_HISTORY_SECTION_TA, CHANGE_HISTORY_SECTION_IL)).click
-    end
+    find_element(key, il_type: :css, tl: CHANGE_HISTORY_SECTION_TA, il: CHANGE_HISTORY_SECTION_IL).click
   end
+
 end
